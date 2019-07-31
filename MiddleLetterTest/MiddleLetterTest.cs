7f4a3a482640e05c4c9631225fab3adb85330b12
Jordan Roberts <jordanroberts@talk21.com>
2019-07-31 19:28:58 +0100

    Passing tests

---
+++ MiddleLetterTest/MiddleLetterTest.cs
@@ -0,0 +1,26 @@
+using GettingMiddleLetters;
+using NUnit.Framework;
+
+namespace MiddleLetterTest
+{
+    public class MiddleLetterTest
+    {
+
+        [TestCase("test", "es")]
+        [TestCase("tests", "s")]
+
+        public void GivenEvenWord_ReturnsTwoLetters(string word, string expected)
+        {
+            MiddleLetter middleletter = new MiddleLetter();
+            string actual = middleletter.GetMiddle(word);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void GivenOddWord_ReturnsOneLetter(string word, string expected)
+        {
+            MiddleLetter middleletter = new MiddleLetter();
+            string actual = middleletter.GetMiddle(word);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
