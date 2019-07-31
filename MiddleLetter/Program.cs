d28649d8c23dd2b01b68f2f5256c5be7569db272
Jordan Roberts <jordanroberts@talk21.com>
2019-07-31 19:10:15 +0100

    Middle letter kata

---
+++ MiddleLetter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MiddleLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word");
+            MiddleLetter middleletter = new MiddleLetter();
+            string input = Console.ReadLine();
+            Console.WriteLine(middleletter.GetMiddle(input));
+        }
+    }
+
+    public class MiddleLetter
+    {
+        public string GetMiddle(string word)
+        {
+            if (word.Length % 2 == 0)
+            {
+                return word.Substring(word.Length / 2 - 1, 2);
+            }
+            else
+            {
+                return word.Substring(word.Length / 2, 1);
+            }
+        }
+    }
+}
+
